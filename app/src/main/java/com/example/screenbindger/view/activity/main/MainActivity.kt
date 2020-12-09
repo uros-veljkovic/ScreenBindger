e991ee31488f8267f446176e6047f0a3d9453fc4
urkeev14 <uros.veljkovic1996@gmail.com>
2020-12-09 20:29:17 +0100

    Create initial folder structure and activities

--- app/src/main/java/com/example/screenbindger/view/activity/main/MainActivity.kt
+++ app/src/main/java/com/example/screenbindger/view/activity/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.screenbindger
+package com.example.screenbindger.view.activity.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.screenbindger.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/screenbindger/view/activity/onboarding/OnboardingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.screenbindger.view.activity.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.screenbindger.R
+
+class OnboardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
