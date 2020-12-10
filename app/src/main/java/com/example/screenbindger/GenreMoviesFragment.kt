ec9e52b9708506d47e226698f340ad44e15bd736
urkeev14 <uros.veljkovic1996@gmail.com>
2020-12-10 11:48:47 +0100

    Add genre movies fragment

---
+++ app/src/main/java/com/example/screenbindger/GenreMoviesFragment.kt
@@ -0,0 +1,17 @@
+package com.example.screenbindger
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class GenreMoviesFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_genre_movies, container, false)
+    }
+
+}
