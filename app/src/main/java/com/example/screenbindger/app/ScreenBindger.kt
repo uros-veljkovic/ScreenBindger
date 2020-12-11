28317fdd309d62089e674c88c5d97471c929215f
urkeev14 <uros.veljkovic1996@gmail.com>
2020-12-11 09:30:19 +0100

    Add access to application context

--- app/src/main/java/com/example/screenbindger/app/ScreenBindger.kt
+++ app/src/main/java/com/example/screenbindger/app/ScreenBindger.kt
@@ -1,8 +1,23 @@
 package com.example.screenbindger.app
 
 import android.app.Application
+import android.content.Context
+import com.example.screenbindger.db.local.entity.UserEntity
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class ScreenBindger : Application() {
+    companion object {
+        var application: ScreenBindger? = null
+
+        fun context(): Context {
+            return application!!.applicationContext
+        }
+
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        application = this
+    }
 }
