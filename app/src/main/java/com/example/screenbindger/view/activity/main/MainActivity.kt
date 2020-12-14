26ddf2fc431c01abac4c5f6c4201a6fb1b6174c4
urkeev14 <uros.veljkovic1996@gmail.com>
2020-12-14 13:05:09 +0100

    Implement bottom navigation and toolbar, and their navigation

--- app/src/main/java/com/example/screenbindger/view/activity/main/MainActivity.kt
+++ app/src/main/java/com/example/screenbindger/view/activity/main/MainActivity.kt
@@ -1,14 +1,54 @@
 package com.example.screenbindger.view.activity.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.example.screenbindger.R
+import com.example.screenbindger.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupGlobalNavigation()
+
+    }
+
+    private fun setupGlobalNavigation() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment?
+        NavigationUI.setupWithNavController(
+            binding.bottomNav,
+            navHostFragment!!.navController
+        )
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.profileFragment -> {
+                findNavController(R.id.nav_host_fragment_activity_main).navigate(R.id.action_global_profileFragment)
+            }
+        }
+        return true
     }
 }
