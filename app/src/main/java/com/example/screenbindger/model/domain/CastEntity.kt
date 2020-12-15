2d28f645ebafd92612676ba434cb8169b894a7f6
urkeev14 <uros.veljkovic1996@gmail.com>
2020-12-15 15:20:03 +0100

    Make CastEntity.class extend BaseObservable.class for purpose of data binding

--- app/src/main/java/com/example/screenbindger/model/domain/CastEntity.kt
+++ app/src/main/java/com/example/screenbindger/model/domain/CastEntity.kt
@@ -1,15 +1,37 @@
 package com.example.screenbindger.model.domain
 
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+import androidx.databinding.library.baseAdapters.BR
 import com.example.screenbindger.model.enums.ItemType
 import com.google.gson.annotations.SerializedName
 
-class CastEntity constructor(
+class CastEntity : BaseObservable(), Item {
+
     @SerializedName("profile_path")
-    val imageUrl: String?,
+    @get: Bindable
+    var imageUrl: String? = null
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.imageUrl)
+        }
+
     @SerializedName("character")
-    val movieName: String?,
+    @get: Bindable
+    var movieName: String? = null
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.movieName)
+        }
+
+
     @SerializedName("original_name")
-    val realName: String?
-): Item{
+    @get: Bindable
+    var realName: String? = null
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.realName)
+        }
+
     override fun getItemType(): ItemType = ItemType.CAST
 }
