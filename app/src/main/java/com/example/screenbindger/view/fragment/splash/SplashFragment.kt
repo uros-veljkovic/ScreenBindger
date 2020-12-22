f338c32fec5fd571c1b4c802d2ffee754e4a4a94
urkeev14 <uros.veljkovic1996@gmail.com>
2020-12-22 16:36:06 +0100

    Add animation to SplashFragment.class and change application font

--- app/src/main/java/com/example/screenbindger/view/fragment/splash/SplashFragment.kt
+++ app/src/main/java/com/example/screenbindger/view/fragment/splash/SplashFragment.kt
@@ -1,5 +1,8 @@
 package com.example.screenbindger.view.fragment.splash
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
@@ -8,20 +11,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.animation.doOnEnd
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.example.screenbindger.R
+import com.example.screenbindger.databinding.FragmentSplashBinding
 import com.example.screenbindger.view.activity.main.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 @InternalCoroutinesApi
 @AndroidEntryPoint
 class SplashFragment : Fragment() {
 
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
     private val viewModel: SplashViewModel by viewModels()
 
     override fun onCreateView(
@@ -29,7 +39,33 @@ class SplashFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+
+        animateView()
+        return binding.root
+    }
+
+    private fun animateView(){
+        val animImageAlpha = ObjectAnimator.ofFloat(binding.ivScreenBindger, "alpha", 1f).also {
+            it.repeatCount = 1
+            it.repeatMode = ValueAnimator.REVERSE
+        }
+
+        val animImageFall = ObjectAnimator.ofFloat(binding.ivScreenBindger, "translationY", -300f, 0f)
+
+        val animTitle = ObjectAnimator.ofFloat(binding.tvScreenBindgerText, "alpha", 1f).also {
+            it.repeatCount = 1
+            it.repeatMode = ValueAnimator.REVERSE
+        }
+
+        AnimatorSet().apply {
+            playTogether(animImageAlpha, animImageFall, animTitle)
+            duration = 2000
+            start()
+            doOnEnd {
+
+            }
+        }
     }
 
     override fun onResume() {
@@ -50,7 +86,7 @@ class SplashFragment : Fragment() {
                 } else {
                     gotoMainActivity()
                 }
-            }, 1000)
+            }, 2000)
         })
     }
 
