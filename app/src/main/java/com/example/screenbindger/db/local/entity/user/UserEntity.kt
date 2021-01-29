13361c02f64adcf7ef7ca0b2cf7d5978de85e19d
Uros Veljkovic <uros.veljkovic1996@gmail.com>
2021-01-29 13:29:03 +0100

    Refactor UserEntity to match needs for Firebase Firestore

--- app/src/main/java/com/example/screenbindger/db/local/entity/user/UserEntity.kt
+++ app/src/main/java/com/example/screenbindger/db/local/entity/user/UserEntity.kt
@@ -1,29 +1,35 @@
 package com.example.screenbindger.db.local.entity.user
 
+import androidx.databinding.Bindable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.screenbindger.BR
 import com.example.screenbindger.db.local.entity.user.observable.UserObservable
+import com.google.firebase.firestore.DocumentId
+import com.google.firebase.firestore.Exclude
 
 @Entity
 data class UserEntity(
-    @PrimaryKey(autoGenerate = true) val _id: Int?,
-    @ColumnInfo(name = "image_uri") val imageUri: String?,
-    @ColumnInfo(name = "full_name") val fullName: String,
-    @ColumnInfo(name = "email") val email: String,
-    @ColumnInfo(name = "password") val password: String,
-    @ColumnInfo(name = "date_of_birth") val dateOfBirth: String?,
-    @ColumnInfo(name = "is_logged_in") val isLoggedIn: Boolean
+    @PrimaryKey(autoGenerate = false)
+    @DocumentId
+    var id: String? = null,
+    var imageUri: String? = null,
+    var fullName: String = "",
+    var email: String = "",
+    @set: Exclude
+    @get:Exclude
+    var password: String = "",
+    var dateOfBirth: String? = null
 ) {
-/*    fun toObservable(): UserObservable {
+    fun toObservable(): UserObservable {
         return UserObservable().also {
-            it.id = _id
+            it.id = id
             it.imageUri = imageUri
             it.fullName = fullName
             it.email = email
             it.dateOfBirth = dateOfBirth
             it.password = password
-            it.isLoggedIn = isLoggedIn
         }
-    }*/
+    }
 }
