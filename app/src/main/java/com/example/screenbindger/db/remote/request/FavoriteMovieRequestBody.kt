4ffa8df86b94634c36d595d272ffb20cc0b9440b
Uros Veljkovic <uros.veljkovic1996@gmail.com>
2021-02-10 15:02:36 +0100

    Remove unnecessary generated classes

--- app/src/main/java/com/example/screenbindger/db/remote/request/FavoriteMovieRequestBody.kt
+++
@@ -1,9 +0,0 @@
-package com.example.screenbindger.db.remote.request
-
-import com.google.gson.annotations.SerializedName
-
-data class FavoriteMovieRequestBody constructor(
-    @SerializedName("media_type") val mediaType: String,
-    @SerializedName("media_id") val mediaId: String,
-    @SerializedName("favorite") val favorite: Boolean
-)
--- app/src/main/java/com/example/screenbindger/db/remote/response/FavoriteMovieResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.example.screenbindger.db.remote.response
-
-import com.google.gson.annotations.SerializedName
-
-data class FavoriteMovieResponse(
-    @SerializedName("status_code") val statusCode: Int,
-    @SerializedName("status_message") val statusMessage: String
-)
