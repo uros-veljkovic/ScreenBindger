7715cd53eb6f82a138a34dc847e29f85a0b2601b
Uros Veljkovic <uros.veljkovic1996@gmail.com>
2021-02-10 10:32:50 +0100

    Create abstract view state for fragments with list

---
+++ app/src/main/java/com/example/screenbindger/view/ListViewState.kt
@@ -0,0 +1,9 @@
+package com.example.screenbindger.view
+
+import com.example.screenbindger.model.domain.MovieEntity
+import com.example.screenbindger.model.state.ListState
+
+abstract class ListViewState<T> (
+    val state: ListState = ListState.Init,
+    val list: List<T>? = null
+)
