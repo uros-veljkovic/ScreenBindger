d38f7f88353e82c35d1015b6e88bde8f17dc3b01
Uros Veljkovic <uros.veljkovic1996@gmail.com>
2021-02-10 10:34:16 +0100

    Refactor TrendingViewState.class to extend ListViewState.class

--- app/src/main/java/com/example/screenbindger/view/fragment/trending/TrendingViewState.kt
+++ app/src/main/java/com/example/screenbindger/view/fragment/trending/TrendingViewState.kt
@@ -2,10 +2,9 @@ package com.example.screenbindger.view.fragment.trending
 
 import com.example.screenbindger.model.domain.MovieEntity
 import com.example.screenbindger.model.state.ListState
+import com.example.screenbindger.view.ListViewState
 
-data class TrendingViewState(
-    val state: ListState,
-    val list: List<MovieEntity>? = null
-){
-
-}
+class TrendingViewState(
+    s: ListState = ListState.Init,
+    l: List<MovieEntity>? = emptyList()
+) : ListViewState<MovieEntity>(s, l)
