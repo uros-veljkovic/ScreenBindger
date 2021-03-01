d57fc6869ee122106b314eb1a37a8fe9702a5152
Uros Veljkovic <uros.veljkovic1996@gmail.com>
2021-03-01 14:57:25 +0100

    Remove redundant state

--- app/src/main/java/com/example/screenbindger/view/ListViewState.kt
+++ app/src/main/java/com/example/screenbindger/view/ListViewState.kt
@@ -3,6 +3,6 @@ package com.example.screenbindger.view
 import com.example.screenbindger.model.state.ListState
 
 abstract class ListViewState<T> (
-    val state: ListState = ListState.Init,
+    val state: ListState = ListState.Fetching,
     val list: List<T>? = null
 )
