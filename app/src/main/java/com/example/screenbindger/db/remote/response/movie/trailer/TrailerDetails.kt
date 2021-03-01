031b5fcd1015a0d98889f4f643fbd75d3f763b51
Uros Veljkovic <uros.veljkovic1996@gmail.com>
2021-03-01 09:58:18 +0100

    Rename MovieTrailerDetails.class to TrailerDetails.class, and MovieTrailersResponse.class to TrailersResponse.class
    This rename action is taken because TrailerDetails.class has the appropriate structure to handle movie trailer details, as well as tv show details.

--- app/src/main/java/com/example/screenbindger/db/remote/response/movie/trailer/TrailerDetails.kt
+++ app/src/main/java/com/example/screenbindger/db/remote/response/movie/trailer/TrailerDetails.kt
@@ -2,7 +2,7 @@ package com.example.screenbindger.db.remote.response.movie.trailer
 
 import com.google.gson.annotations.SerializedName
 
-data class MovieTrailerDetails(
+data class TrailerDetails(
     @SerializedName("id") val id: String? = null,
     @SerializedName("key") val key: String? = null,
     @SerializedName("name") val name: String? = null,
--- app/src/main/java/com/example/screenbindger/db/remote/response/movie/trailer/TrailersResponse.kt
+++ app/src/main/java/com/example/screenbindger/db/remote/response/movie/trailer/TrailersResponse.kt
@@ -2,8 +2,8 @@ package com.example.screenbindger.db.remote.response.movie.trailer
 
 import com.google.gson.annotations.SerializedName
 
-data class MovieTrailersResponse(
+data class TrailersResponse(
     @SerializedName("id") val id: Int,
-    @SerializedName("results") val list: List<MovieTrailerDetails>
+    @SerializedName("results") val list: List<TrailerDetails>
 ) {
 }
