8154f48aa4b1787c0fdf9a31681858fcb83b9c3b
Uros Veljkovic <uros.veljkovic1996@gmail.com>
2021-03-02 14:23:01 +0100

    Remove unused classes

--- app/src/main/java/com/example/screenbindger/view/fragment/SessionEventObservable.kt
+++
@@ -1,7 +0,0 @@
-package com.example.screenbindger.view.fragment
-
-import com.example.screenbindger.model.state.SessionState
-import com.example.screenbindger.util.event.EventObservable
-
-class SessionEventObservable: EventObservable<SessionState>() {
-}
--- app/src/main/java/com/example/screenbindger/view/fragment/TokenStateObservable.kt
+++
@@ -1,7 +0,0 @@
-package com.example.screenbindger.view.fragment
-
-import com.example.screenbindger.model.state.TokenState
-import com.example.screenbindger.util.event.EventObservable
-
-class AuthorizationStateObservableTokenEventObservable : EventObservable<TokenState>() {
-}
