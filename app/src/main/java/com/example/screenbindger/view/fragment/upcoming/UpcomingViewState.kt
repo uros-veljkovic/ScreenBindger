d73f6a46ffe93793facbc2abad9d843b2a39551e
Uros Veljkovic <uros.veljkovic1996@gmail.com>
2021-03-04 14:21:55 +0100

    Modify UpcomingViewState to contain data-holder classes

--- app/src/main/java/com/example/screenbindger/view/fragment/upcoming/UpcomingViewState.kt
+++ app/src/main/java/com/example/screenbindger/view/fragment/upcoming/UpcomingViewState.kt
@@ -2,9 +2,30 @@ package com.example.screenbindger.view.fragment.upcoming
 
 import com.example.screenbindger.model.domain.movie.ShowEntity
 import com.example.screenbindger.model.state.ListState
+import com.example.screenbindger.util.event.Event
 import com.example.screenbindger.view.ListViewState
 
-class UpcomingViewState(
-    state: ListState = ListState.Fetching,
-    list: List<ShowEntity>? = emptyList()
-) : ListViewState<ShowEntity>(state, list)
+sealed class UpcomingViewState {
+    object Init: UpcomingViewState()
+    object Fetching : UpcomingViewState()
+
+    sealed class Fetched(
+        val list: List<ShowEntity>,
+        val currentPage: Int,
+        val totalPages: Int
+    ) : UpcomingViewState() {
+        class Movies(
+            list: List<ShowEntity>,
+            page: Int,
+            totalPages: Int
+        ) : Fetched(list, page, totalPages)
+
+        class TvShows(
+            list: List<ShowEntity>,
+            page: Int,
+            totalPages: Int
+        ) : Fetched(list, page, totalPages)
+    }
+
+    data class NotFetched(val message: Event<String>) : UpcomingViewState()
+}
