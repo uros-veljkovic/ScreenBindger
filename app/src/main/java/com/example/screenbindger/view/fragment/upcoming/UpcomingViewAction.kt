653c286226a233e74d6e55a348eb9f19124e9c91
Uros Veljkovic <uros.veljkovic1996@gmail.com>
2021-03-04 15:40:21 +0100

    Create intent-based actions to fetch previous/next page of movies/tv shows

--- app/src/main/java/com/example/screenbindger/view/fragment/upcoming/UpcomingViewAction.kt
+++ app/src/main/java/com/example/screenbindger/view/fragment/upcoming/UpcomingViewAction.kt
@@ -3,4 +3,6 @@ package com.example.screenbindger.view.fragment.upcoming
 sealed class UpcomingViewAction {
     object FetchMovies : UpcomingViewAction()
     object FetchTvShows : UpcomingViewAction()
+    object GotoNextPage : UpcomingViewAction()
+    object GotoPreviousPage : UpcomingViewAction()
 }
